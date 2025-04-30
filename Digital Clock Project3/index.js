fa78ea6724a119653dffa8e62038f2d72899ecb8
mehak-1903 <mehakaggarwal1019@gmail.com>
2025-04-30 12:00:50 +0530

    Adding comment

--- Digital Clock Project3/index.js
+++ Digital Clock Project3/index.js
@@ -1,6 +1,6 @@
 const clock = document.getElementById('clock');
 
 setInterval(()=>{
-    let date = new Date();
-    clock.innerHTML = date.toLocaleTimeString();
+    let date = new Date();              //new Date() => current time lata hai
+    clock.innerHTML = date.toLocaleTimeString();                //toLocaleTimeString() => converted time into readable form
 }, 1000)
