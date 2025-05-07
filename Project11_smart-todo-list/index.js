fc7cc4a85635f98895025fe02bab1e506725e6aa
mehak-1903 <mehakaggarwal1019@gmail.com>
2025-05-07 12:03:44 +0530

    Adding edit and strikethrough functionality and also added 3buttons

--- Project11_smart-todo-list/index.js
+++ Project11_smart-todo-list/index.js
@@ -4,15 +4,28 @@ const taskList = document.querySelector('.list-container');
 
 let tasks = [];             // All tasks are stored in this empty list.
 
+//  saved values 
+
+function saveTask(){
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+const savedTask = localStorage.getItem('tasks');
+if(savedTask){
+    tasks = JSON.parse(savedTask);
+    showTasks();
+}
+
 addBtn.addEventListener('click', () => {
     
     // Input values ko lenge
     const taskText = input.value.trim();
 
     if(taskText !== ''){
-        tasks.push(taskText);     // Adding to the list
+        tasks.push({text: taskText, done: false});     // Adding to the list--- stored as an object
         input.value = '';         // clear input box
 
+        saveTask();
         showTasks();              // showing tasks
     }
 
@@ -26,7 +39,11 @@ function showTasks(){
     tasks.forEach((task , index) => {
         const li = document.createElement('li');
 
-        li.textContent = task;     // li mei tasks dalra hai
+        li.textContent = task.text;     // li mei tasks dalra hai
+
+        if(task.done){
+            li.style.textDecoration =  'line-through';
+        }
 
         // Creating and Adding Delete Button to the list
         
@@ -36,11 +53,41 @@ function showTasks(){
         deleteBtn.addEventListener('click', () => {
             tasks.splice(index, 1);       // remove from list
 
+            saveTask();
             showTasks();         // updated list
         })
+
+        // Done Task
+        const doneBtn = document.createElement('i');
+        doneBtn.className = 'fa-solid fa-check';
+        doneBtn.style.marginRight = '38px';
+
+        doneBtn.addEventListener('click', () => {
+            tasks[index].done = !tasks[index].done;
+
+            saveTask();
+
+            // Only update current li instead of full re-render
+            li.style.textDecoration = tasks[index].done ? 'line-through' : 'none';
+        })
+
+        // Editing
+        const editText = document.createElement('i');
+        editText.className = 'fa-solid fa-pen';
+        editText.style.marginRight = '75px';
+
+        editText.addEventListener('click', () => {
+            const newText = prompt("Enter your Text: ", task.text);
+            if(newText !== null && newText.trim() !== ''){
+                tasks[index].text = newText.trim();
+            }
+            saveTask();
+            showTasks()
+        })
         
+        li.appendChild(editText);
+        li.appendChild(doneBtn);
         li.appendChild(deleteBtn);      // adding icon with the list
         taskList.appendChild(li);         // showing to the list
-        console.log(deleteBtn)
     })
 }
